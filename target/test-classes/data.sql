db7867ef4ee86ec738a87390fb6f10ffa9d02f43
ansesolo <65455374+ansesolo@users.noreply.github.com>
2021-11-22 12:58:48 +0100

    Delete target/test-classes directory

--- target/test-classes/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO short_urls (id, short_url, url) VALUES (1, '12345', 'http://www.google.com');
-INSERT INTO short_urls (id, short_url, url) VALUES (2, '67890', 'http://www.leprogres.fr');
